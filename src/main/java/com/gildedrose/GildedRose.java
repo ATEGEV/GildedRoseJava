32de87138a83a9ab57f227ff2da76aaf46aed755
BnwoTraining <bnwoadres@gmail.com>
2022-03-09 07:14:56 +0100

    GildedRose.java refactored;

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,61 +1,72 @@
 package com.gildedrose;
 
 class GildedRose {
+
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String AGED_BRIE = "Aged Brie";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+
+    private void setQualityToZero(Item item) {
+        item.quality = 0;
+    }
+
+    private void decreaseSellin(Item item) {
+        item.sellIn--;
+    }
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+        //*
+        for (Item item : items) {
+            switch (item.name) {
+                case SULFURAS:
+                    break;
+                case BACKSTAGE_PASS:
+                    if (item.sellIn <= 0) {
+                        setQualityToZero(item);
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        increaseQuality(item);
+                        if (item.sellIn < 11) {
+                            increaseQuality(item);
+                        }
+                        if (item.sellIn < 6) {
+                            increaseQuality(item);
+                        }
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    decreaseSellin(item);
+                    break;
+                case AGED_BRIE:
+                    //deze increase staat eigenlijk niet in de resuirements, maar iw wel zo gïmplementeerd in de bestaande code
+                    if (item.sellIn <= 0) {
+                        increaseQuality(item);
                     }
-                }
+                    increaseQuality(item);
+                    decreaseSellin(item);
+                    break;
+                default:
+                    decreaseQuality(item);
+                    if (item.sellIn <= 0) {
+                        decreaseQuality(item);
+                    }
+                    decreaseSellin(item);
+                    break;
             }
         }
     }
